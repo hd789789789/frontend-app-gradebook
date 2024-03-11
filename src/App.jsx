d1497991f1a746e28925d53f6631714eb019f1a4
hd789789789 <65296761+hd789789789@users.noreply.github.com>
2024-03-11 09:57:19 +0700

    Add Vietnamese Language

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import { AppProvider } from '@edx/frontend-platform/react';
+import { AppProvider } from "@edx/frontend-platform/react";
 
-import Footer from '@edx/frontend-component-footer';
-import Header from '@edx/frontend-component-header';
+// import Footer from '@edx/frontend-component-footer';
+import Header from "@edx/frontend-component-header";
 
-import store from 'data/store';
-import GradebookPage from 'containers/GradebookPage';
-import './App.scss';
-import Head from './head/Head';
+import store from "data/store";
+import GradebookPage from "containers/GradebookPage";
+import "./App.scss";
+import Head from "./head/Head";
 
 const App = () => (
-  <AppProvider store={store}>
-    <Head />
-    <div>
-      <Header />
-      <main>
-        <Routes>
-          <Route
-            path="/:courseId"
-            element={<GradebookPage />}
-          />
-        </Routes>
-      </main>
-      <Footer logo={process.env.LOGO_POWERED_BY_OPEN_EDX_URL_SVG} />
-    </div>
-  </AppProvider>
+    <AppProvider store={store}>
+        <Head />
+        <div>
+            <Header />
+            <main>
+                <Routes>
+                    <Route path="/:courseId" element={<GradebookPage />} />
+                </Routes>
+            </main>
+            {/* <Footer logo={process.env.LOGO_POWERED_BY_OPEN_EDX_URL_SVG} /> */}
+        </div>
+    </AppProvider>
 );
 
 export default App;
